48b01302b3204426db2cfa1b4309e204af75d1b8
AaronADeveloper <aaronadeveloper@gmail.com>
2020-10-03 16:43:47 -0400

    completed rendering of chart component with data (however, API currently down)

--- src/API/index.js
+++ src/API/index.js
@@ -14,5 +14,21 @@ export const fetchData = async () => {
       deaths,
       lastUpdate,
     };
-  } catch (error) {}
+  } catch (error) {
+    return error;
+  }
+};
+
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+
+    return data.map((dailyData) => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate,
+    }));
+  } catch (error) {
+    return error;
+  }
 };
--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { Cards, Chart, CountryPicker } from "./components";
 import styles from "./App.module.css";
-import { fetchData } from "./API";
+import { fetchData } from "./API/";
 
 class App extends React.Component {
   state = {
@@ -19,7 +19,7 @@ class App extends React.Component {
     return (
       <div className={styles.container}>
         <Cards data={data} />
-        <Chart />
+        <Chart data={data} />
         <CountryPicker />
       </div>
     );
